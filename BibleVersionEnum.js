662f9deca5ff130c3fa3029b3f83f68b8b729fbe
tonussi <lptonussi@gmail.com>
2018-12-24 02:45:06 -0200

    Start project

---
+++ BibleVersionEnum.js
@@ -0,0 +1,10 @@
+const BibleVersionEnum = {
+  EMTV: "EMTV",
+  BYZ: "BYZ",
+  WPNT: "WPNT",
+  ACF: "ACF"
+}
+
+Object.freeze(BibleVersionEnum);
+
+module.exports.BibleVersionEnum = BibleVersionEnum;
